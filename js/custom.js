e937210c6c8017f1f705a8108b7ecba67c4b69f0
david-ziffity <david.francis@ziffity.com>
2022-06-02 14:03:57 +0530

    Code Formatted

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-	AOS.init();
-	$('#navToggle').on('click', function(){
-		$('body').toggleClass('nav-active');
-	});
+$(document).ready(function () {
+    AOS.init();
+    $('#navToggle').on('click', function () {
+        $('body').toggleClass('nav-active');
+    });
 });
